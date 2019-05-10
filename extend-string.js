dd6db8f48dd2e7199b23c6819423201fc903730a
seun jay <seunjay92@gmail.com>
2019-05-10 22:34:06 +0100

    Define toUpper method on the String prototype

--- extend-string.js
+++ extend-string.js
@@ -2,3 +2,15 @@ String.prototype.hasVowels = function() {
   let regEx = /[aeiou]/g;
   return regEx.test(this)? true : false
 }
+
+String.prototype.toUpper = function() {
+  let result = '';
+  for(index = 0; index < this.length; index++){
+    let characterCode = this.charCodeAt(index);
+    if(characterCode >= 97 && characterCode <= 122){
+      characterCode -= 32
+    }
+    result += String.fromCharCode(characterCode);
+  }
+  return result
+}
