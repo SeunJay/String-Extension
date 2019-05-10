d222db38408a428dd6dfdad22f421f2f965b0961
seun jay <seunjay92@gmail.com>
2019-05-10 22:19:33 +0100

    Initialize hasVowels method to fail

--- test/extend-string.test.js
+++ test/extend-string.test.js
@@ -1,1 +1,8 @@
 let string = require("../extend-string");
+let demoString = 'seun';
+
+describe("Extended String-methods", () => {
+  it("Should return a boolean if a string contains a vowel", () => {
+    expect(demoString.hasVowels()).toBeTruthy();
+  })
+})
